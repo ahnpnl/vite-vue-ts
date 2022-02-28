6be8fc63cf99ed09dda8f91175daf060fb166f8d
anh.pham <anh.pham@cm.com>
2022-02-28 22:01:32 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const placeHolder = 'foo';
+const label = 'Bar';
+</script>
+
+<template>
+    <input
+        id="product-name"
+        :^placeholder="placeHolder"
+        ^required
+        :data-label="label"
+        ^type="text"
+    />
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
